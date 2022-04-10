b6ec55e46bce5bca7a5183eeebd80ae1565d4d00
lpilas24 <leiva20152@gmail.com>
2022-04-10 13:24:35 -0500

    Simple form and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import LoginPage from './pages/Login/LoginPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./pages/Login/LoginPage";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-
-
+          <Route path="/" element={<LoginPage />} />.
+          <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,8 +1,74 @@
+import { Button, TextField, Container, Box, Alert } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+import logo from "../../assets/clinica-logo.png";
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 const LoginPage = () => {
-    return (
-        <div>LoginPage</div>
-    )
-}
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-export default LoginPage
+  const onSubmit = (data: any) => {
+    console.log(data);
+    //llamar a la api
+    navigate("/dashboard");
+  };
+
+  return (
+    <>
+      <Container maxWidth="xs">
+        <h1>Bienvenido a</h1>
+
+        <Box mb={3}>
+          <img src={logo} alt="Logo de la clínica" />
+        </Box>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box mb={2}>
+            <TextField
+              {...register("email", {
+                required: true,
+                pattern:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i,
+              })}
+              variant="outlined"
+              label="email"
+              fullWidth
+              autoComplete="email"
+              autoFocus
+            />
+            {errors.email && (
+              <Alert severity="error">Ingrese un email válido</Alert>
+            )}
+          </Box>
+          <Box mb={2}>
+            <TextField
+              {...register("password", { required: true })}
+              variant="outlined"
+              label="contraseña"
+              fullWidth
+              type="password"
+            />
+            {errors.password && (
+              <Alert severity="error">Este campo no puede estar vacío</Alert>
+            )}
+          </Box>
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Ingresar
+          </Button>
+        </form>
+      </Container>
+    </>
+  );
+};
+
+export default LoginPage;
